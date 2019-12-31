6b748ef94bcedc055d7e70a7d3596ece04c86719
Joe <josephedwardwork@gmail.com>
2019-12-31 01:21:37 -0500

    commit

--- js/init.js
+++ js/init.js
@@ -2,11 +2,35 @@ $(document).ready(function() {
 // reset();
 // addSlidesToCarosel();
 // addProjectDropdown();
-bindClickEvents();
+// bindBreakPointEvents();
 
+bindClickEvents();
 
 });  
 
 function reset(){
     $("html").reset("all","unset");
 }
+
+
+// function bindBreakPointEvents(){
+
+// $(window).on('lessThan-md', function() {
+//     // outputListenExecuteHide($outputListenExecute);
+//     // $outputListenExecute.find('.output--listen-execute__sm').show();
+//     $(".blackSpace").hide();
+// });
+
+// $(window).bind('breakpoint-change', function(e) {
+//     var $outputListening = $('#output-listening');
+//     $outputListening.find('#output-listening--current').html(e.to);
+//     $outputListening.find('#output-listening--previous').html(e.from);
+//     console.log("breakpoint change");
+//   });
+
+//   $(window).breakpoints('inside', 'sm', function() {
+//     outputListenExecuteHide($outputListenExecute);
+//     $outputListenExecute.find('.output--listen-execute__sm').show();
+//   });
+
+// }
